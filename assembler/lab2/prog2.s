6ef143757f5f588a6df76f19467f86279ba4777a
Cronion <ohmytarik@mail.ru>
2022-05-05 23:42:46 +0300

    no sorting for last 2 lines

--- assembler/lab2/prog2.s
+++ assembler/lab2/prog2.s
@@ -60,27 +60,25 @@ reset_index:
 	mov x5, #0
 	add x6, x6, #1
 	cmp x6, x1
-	bge heapsort_set_matr
+	bge heapsort_set_index
 	b process_line
 
 //sort the array of the mins
-heapsort_set_matr:
-	mov x4, x3 //beginning of the matrix
 
 heapsort_set_index:
-	//cmp x6, x1 
-	//beq exit //exit if processed all lines
-	//add x4, x4, x0, lsl #2 //get next line beginning
 	lsr x5, x1, #1 //get i = size / 2
 	sub x6, x1, #1
 	sub x13, x0, #1 //max index in the line
+	mov x17, x1 //index of the last line
 	
 heapsort0: //beginning of the sort
+	mov x14, #0 //counter of the moved elems
 	cbz x5, heapsort1
+	sub x17, x17, #1
 	sub x5, x5, #1
 	b heapsort2	
 	
-heapsort1: //processing last 2 elems
+heapsort1: //processing last 2 lines
 	cbz x6, exit //if(mins.length == 0) => exit
 	ldr w7, [x2, x5, lsl #2]
 	ldr w8, [x2, x6, lsl #2]
@@ -88,6 +86,7 @@ heapsort1: //processing last 2 elems
 	str w7, [x2, x6, lsl #2]
 	sub x6, x6, #1
 	cbz x6, exit
+	//cbz x6, prep_line_index
 
 heapsort2: //load value from index
 	ldr w7, [x2, x5, lsl #2] //get parent tree node, i1 = x5
@@ -113,8 +112,9 @@ heapsort3: //
 	//x7 - x5; x8 - x10
 	mov x8, x12
 
+
+
 heapsort4: //
-	//mov x14, #0 //counter of moved elems
 	cmp x7, x8
 	.ifdef ascending
 	bge heapsort5
@@ -122,40 +122,39 @@ heapsort4: //
 	ble heapsort5
 	.endif //i = x10 line should be moved
 	
-	//mov x11 x10 
-	//mul x //get address of the beginning of the line 
+	mov x11, x10 
 	str w8, [x2, x9, lsl #2]
-	//b move_matrix_lines
-	b heapsort3
-
+	b prep_line_index
 heapsort5: //store the top of the heap
 	//i = x5 line should be moved
 
-	//mov x11 x5
+	mov x11, x5
 	str w7, [x2, x9, lsl #2]
 	
+prep_line_index:
+	add x4, x3, x17, lsl #2 //addr of the last, not sorted line
+	add x18, x3, x11, lsl #2
+
 move_matrix_lines:
-	//moving lines {...}
 	cmp x14, x13
 	bge next_iteration
-	ldr	x15, [x4, x14, lsl #2]//get 
-	ldr	x16, [x4, x14, lsl #2]//
-	str	x15, [x4, x14, lsl #2]
-	str	x16, [x4, x14, lsl #2]
+	ldr	w15, [x4, x14, lsl #2]//get 
+	ldr	w16, [x18, x14, lsl #2]//
+	str	w15, [x18, x14, lsl #2]
+	str	w16, [x4, x14, lsl #2]
 	add x14, x14, #1
 	b move_matrix_lines
 
 
 next_iteration:
-    //mov x4, x3
-	//cmp x7, x8
-	//.ifdef ascending
-	//bge heapsort0
-	//.else
-	//ble heaposrt0
-	//.endif
-	//b heapsort3
-	b heapsort0
+    mov x4, x3
+	cmp x7, x8
+	.ifdef ascending
+	bge heapsort0
+	.else
+	ble heaposrt0
+	.endif
+	b heapsort3
 
 exit:
 	mov x5, #0
