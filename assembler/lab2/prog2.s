bdc1594674a11a62f564139fd054a2663b44ea83
Cronion <ohmytarik@mail.ru>
2022-05-06 19:29:26 +0300

    .

--- assembler/lab2/prog2.s
+++ assembler/lab2/prog2.s
@@ -14,6 +14,8 @@ matrix: //;matrix
 	.word  34, 44, 67, 88, 5
 	.word  3, 23, 74, 7, 4
 
+sort_mins:
+	.skip 40
 mins:
 	.skip 40
 	.text
@@ -26,13 +28,14 @@ _start:
 	ldr w0, [x5]
 	adr x5, num_rows
 	ldr w1, [x5]
-	adr x2, mins
+	adr x2, sort_mins
+	adr x13, mins
 	adr x3, matrix
 	mov x4, x3
 	mov x5, #0 // i
 	mov x6, #0 // j
 
-//finding mins for each line
+//finding sort_mins for each line
  process_line:
 	cmp x5, x0
 	bge reset_index //
@@ -56,6 +59,7 @@ process_first_elem:
 
 reset_index:
 	str w7, [x2, x6, lsl #2] //str x3, x7 
+	str w7, [x13, x6, lsl #2]
 	add x4, x4, x5, lsl #2
 	mov x5, #0
 	add x6, x6, #1
@@ -63,30 +67,27 @@ reset_index:
 	bge heapsort_set_index
 	b process_line
 
-//sort the array of the mins
+//sort the array of the sort_mins
 
 heapsort_set_index:
+
 	lsr x5, x1, #1 //get i = size / 2
 	sub x6, x1, #1
 	sub x13, x0, #1 //max index in the line
-	sub x17, x17, #1 //index of the line that is going to be moved
 	
 heapsort0:
-	mov x14, #0 //counter of the moved elems in line
-
 	cbz x5, heapsort1 //if(x5 == 0) => heapsort1
 	sub x5, x5, #1
 	b heapsort2	
 	
-heapsort1: //processing last 2 lines
-	cbz x6, exit //if(mins.length == 0) => exit
+heapsort1: //store top of the heap
+	cbz x6, exit //if(sort_mins.length == 0) => exit
 	ldr w7, [x2, x5, lsl #2]
 	ldr w8, [x2, x6, lsl #2]
 	str w8, [x2, x5, lsl #2]
 	str w7, [x2, x6, lsl #2]
 	sub x6, x6, #1
 	cbz x6, exit
-	//cbz x6, prep_line_index
 
 heapsort2: //load value from index
 	ldr w7, [x2, x5, lsl #2] //get parent tree node, i1 = x5
@@ -109,9 +110,8 @@ heapsort3: //
 	ble heapsort4
 	.endif
 	add x10, x10, #1 //as a result we have 2 indexes: x5 and x10
-	//x7 - x5; x8 - x10
 	mov x8, x12
-
+	//x7 - x5; x8 - x10
 
 heapsort4: //
 	cmp x7, x8
@@ -123,61 +123,14 @@ heapsort4: //
 	str w8, [x2, x9, lsl #2]
 	b heapsort3
 
-heapsort5: //store the top of the heap
-	mov x11, x9
-	str w7, [x2, x9, lsl #2]
-
-move_matrix_lines:
-	
+heapsort5:
+	str w7, [x2, x9, lsl #2]	
 	b heapsort0
 
+move_matrix_lines:
 
 
 
-
-
-
-
-@ heapsort4: //
-@ 	cmp x7, x8
-@ 	.ifdef ascending
-@ 	bge heapsort5
-@ 	.else
-@ 	ble heapsort5
-@ 	.endif //i = x10 line should be moved
-@ 	mov x11, x10 
-@ 	str w8, [x2, x9, lsl #2]
-@ 	b prep_line_index
-
-@ heapsort5: //store the top of the heap
-@ 	//i = x5 line should be moved
-@ 	mov x11, x5
-@ 	str w7, [x2, x9, lsl #2]
-	
-@ prep_line_index:
-@ 	add x4, x3, x17, lsl #2 //addr of the last, not sorted line
-@ 	add x18, x3, x11, lsl #2
-
-@ move_matrix_lines:
-@ 	cmp x14, x13
-@ 	bge next_iteration
-@ 	ldr	w15, [x4, x14, lsl #2]//get 
-@ 	ldr	w16, [x18, x14, lsl #2]//
-@ 	str	w15, [x18, x14, lsl #2]
-@ 	str	w16, [x4, x14, lsl #2]
-@ 	add x14, x14, #1
-@ 	b move_matrix_lines
-
-
-@ next_iteration:
-@     mov x4, x3
-@ 	cmp x7, x8
-@ 	.ifdef ascending
-@ 	bge heapsort0
-@ 	.else
-@ 	ble heapsort0
-@ 	.endif
-@ 	b heapsort3
 
 exit:
 	mov x5, #0
