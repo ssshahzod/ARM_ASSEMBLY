0e1d31c8c0157eba3c764a2e2bd723fcdd6fbc92
ssshahzod <55853125+ssshahzod@users.noreply.github.com>
2022-08-26 11:00:13 +0300

    Update prog3.s

--- assembler/lab3/prog3.s
+++ assembler/lab3/prog3.s
@@ -1,165 +1,198 @@
-	.arch armv8-a
-//	Copy all odd words in each string from file1 to file2
-	.data
-mes1:
-	.string	"Input filename for read\n"
-	.equ	mes1len, .-mes1
-ans:
-	.skip	3
-name1:
-	.skip	1024
-	.align	3
-fd1:
-	.skip	8
-	.text
-	.align	2
-	.global _start	
-	.type	_start, %function
+        .arch armv8-a
+//      Print text file
+        .data
+errmes1:
+        .string "Usage: "
+        .equ    errlen1, .-errmes1
+errmes2:
+        .string " filename\n"
+        .equ    errlen2, .-errmes2
+resstr:
+        .skip 4096
+
+        .text
+        .align  2
+        .global _start
+        .type   _start, %function
 _start:
-	ldr	x0, [sp]
-	cmp	x0, #1
-	beq	0f
-	mov	x0, #0
-	b	8f
+        ldr     x0, [sp]
+        cmp     x0, #2
+        beq     2f
+        mov     x0, #2
+        adr     x1, errmes1
+        mov     x2, errlen1
+        mov     x8, #64
+        svc     #0
+        mov     x0, #2
+        ldr     x1, [sp, #8]
+        mov     x2, #0
 0:
-	mov	x0, #1 //file descriptor stdout
-	adr	x1, mes1
-	mov	x2, mes1len
-	mov	x8, #64
-	svc	#0
-	mov	x0, #0 //file descriptor stdin
-	adr	x1, name1
-	mov	x2, #1024
-	mov	x8, #63
-	svc	#0
-	cmp	x0, #1
-	ble	1f //if error
-	cmp	x0, #1024
-	blt	2f
-
+        ldrb    w3, [x1, x2]
+        cbz     w3, 1f
+        add     x2, x2, #1
+        b       0b
 1:
-	mov	x0, #1
-	b	8f
+        mov     x8, #64
+        svc     #0
+        mov     x0, #2
+        adr     x1, errmes2
+        mov     x2, errlen2
+        mov     x8, #64
+        svc     #0
+        mov     x0, #1
+        b       3f
 2:
-	sub	x2, x0, #1
-	mov	x0, #-100 //open file to read
-	adr	x1, name1
-	strb	wzr, [x1, x2] //move 0-byte to the end of the string
-	mov	x2, #0
-	mov	x8, #56
-	svc	#0
-	cmp	x0, #0
-	blt writeerr //x0 < 0 is error
-	adr	x1, fd1
-	str	x0, [x1]
-	bl work
-
-	
-8:
-	bl	writeerr
-	mov	x0, #1
-	b	1f
-
-0:
-	bl	writeerr
-	adr	x0, fd1
-	ldr	x0, [x0]
-	mov	x8, #57
-	svc	#0
-	mov	x0, #1
-1:
-	mov	x8, #93
-	svc	#0
-	.size	_start, .-_start
-	.type	work, %function
-	.equ	f1, 16
-	.equ	f2, 24
-	.equ	tmp, 32
-	.equ	flg, 40
-	.equ	wrd, 44
-	.equ	bufin, 48
-	.equ	bufout, 4144
-	.text
-	.align	2
-work: 
-
-
-8:
-	str	x0, [x29, tmp]
-	ldr	x0, [x29, f2]
-	mov	x1, #0
-	mov	x8, #46
-	svc	#0
-	ldr	x0, [x29, tmp]
-
-
-	.size	work, .-work
-	.type	writeerr, %function
-	.data
-usage:
-	.string	"Program does not require parameters\n"
-	.equ	usagelen, .-usage
-nofile:
-	.string	"No such file or directory\n"
-	.equ	nofilelen, .-nofile
-permission:
-	.string	"Permission denied\n"
-	.equ	permissionlen, .-permission
-isdir:
-	.string	"Is a directory\n"
-	.equ	isdirlen, .-isdir
-toolong:
-	.string	"File name too long\n"
-	.equ	toolonglen, .-toolong
-readerror:
-	.string "Error readig filename\n"
-	.equ	readerrorlen, .-readerror
-unknown:
-	.string	"Unknown error\n"
-	.equ	unknownlen, .-unknown
-	.text
-	.align	2
-writeerr:
-	cbnz	x0, 0f
-	adr	x1, usage
-	mov	x2, usagelen
-	b	6f
-0:
-	cmp	x0, #-2
-	bne	1f
-	adr	x1, nofile
-	mov	x2, nofilelen
-	b	6f
-1:
-	cmp	x0, #-13
-	bne	2f
-	adr	x1, permission
-	mov	x2, permissionlen
-	b	6f
-2:
-	cmp	x0, #-21
-	bne	3f
-	adr	x1, isdir
-	mov	x2, isdirlen
-	b 	6f
+        ldr     x0, [sp, #16]
+        bl      work
 3:
-	cmp	x0, #-36
-	bne	4f
-	adr	x1, toolong
-	mov	x2, toolonglen
-	b	6f
+        mov     x8, #93
+        svc     #0
+        .size   _start, .-_start
+        .type   work, %function
+        .text
+        .align  2
+        .equ    filename, 16
+        .equ    fd, 24
+        .equ    buf, 32
+work:
+        //prepare new stackframe
+        mov     x16, #4128
+        sub     sp, sp, x16
+        //save 
+        stp     x29, x30, [sp]
+        mov     x29, sp
+        str     x0, [x29, filename]
+        mov     x1, x0
+
+        //open file
+        mov     x0, #-100
+        mov     x2, #0
+        mov     x8, #56
+        svc     #0
+        cmp     x0, #0
+
+        bge     0f
+        bl      writeerr
+        b       8f //leave function
+0:
+        str     x0, [x29, fd]
+1:
+        //read from file
+        ldr     x0, [x29, fd]
+        add     x1, x29, buf
+        mov     x2, #4096
+        mov     x8, #63
+        svc     #0
+        cmp     x0, #0
+        beq     7f //close file
+        adr x3, resstr
+        mov w10, ' '
+        bgt     2f
+
+        str     x0, [sp, #-16]!
+        ldr     x0, [x29, fd]
+        mov     x8, #57
+        svc     #0
+        ldr     x0, [sp], #16
+        bl      writeerr
+        mov     x0, #1
+        b       8f //leave function
+2:
+        //
+        //delete some words from x1 and put result string in x3
+        //x4 addr of the beginning of the word
+        //x5 addr of the end of the word
+        mov x4, x1
+        ldrb w8, [x1], #1
+        cmp w8, '\t' //get first letter of the word 
+        //!!!!!here we are saving the letter itself, not the addres of it
+        beq 2b  
+        cmp w8, '\n'
+        beq 2b
+        cmp w8, ' '
+        beq 2b
+
+3:
+
+        ldrb w9, [x1], #1
+        cmp w9, '\t'
+    beq 4f
+        cmp w9, '\n'
+        beq 4f
+        cmp w9, ' '
+        beq 4f
+        b 3b
+
 4:
-	cmp	x0, #1
-	bne	5f
-	adr	x1, readerror
-	mov	x2, readerrorlen
-	b	6f
+        //get last letter of the first word
+        ldrb w5, [x1, #-2]!
+        mov x5, x1
+
+        cmp w10, ' '
+        beq 5f
+        //cmp w5, w10
+        //if equals we just skip the word 
+        //else we put it in the result string
 5:
-	adr	x1, unknown
-	mov	x2, unknownlen
+        //save the last letter of the first word
+        mov w10, w9
+        //b 2b
+
 6:
-	mov	x0, #2
-	mov	x8, #64
-	svc	#0
-	ret
-	.size	writeerr, .-writeerr
+        //write the text to stdout
+        mov     x2, x0
+        mov     x0, #1
+        //add   x1, x29, buf
+        mov x1, x3
+        mov     x8, #64
+        svc     #0
+        b       1b
+7:
+        //close the file
+        ldr     x0, [x29, fd]
+        mov     x8, #57
+        svc     #0
+        mov     x0, #0
+8:
+        //close function
+        ldp     x29, x30, [sp]
+        mov     x16, #4128
+        add     sp, sp, x16
+        ret
+        .size   work, .-work
+        .type   writeerr, %function
+
+
+        .data
+nofile:
+        .string "No such file or directory\n"
+        .equ    nofilelen, .-nofile
+permission:
+        .string "Permission denied\n"
+        .equ    permissionlen, .-permission
+unknown:
+        .string "Unknown error\n"
+        .equ    unknownlen, .-unknown
+        .text
+        .align  2
+writeerr:
+        cmp     x0, #-2
+        bne     0f
+        adr     x1, nofile
+        mov     x2, nofilelen
+        b       2f
+0:
+        cmp     x0, #-13
+        bne     1f
+        adr     x1, permission
+        mov     x2, permissionlen
+        b       2f
+1:
+        adr     x1, unknown
+        mov     x2, unknownlen
+2:
+        mov     x0, #2
+        mov     x8, #64
+        svc     #0
+        ret
+        .size   writeerr, .-writeerr
