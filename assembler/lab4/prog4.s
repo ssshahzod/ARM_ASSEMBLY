cd8667ff937349eccdfe163f379028a0c6a3ab65
Dozen Sh <ohmytarik@mail.ru>
2022-08-31 10:46:15 +0300

    opening file correctly, and closing it

--- assembler/lab4/prog4.s
+++ assembler/lab4/prog4.s
@@ -8,6 +8,10 @@ mes2:
 	.string	"%lf" //input format
 mes3:
 	.string	"cos(%.17g) = %.17g\n"
+mode:
+	.string "w"
+filestr:
+	.string "%d:  %.17g"
 
 testmes:
 	.string "Filename entered: %s\n"
@@ -36,14 +40,14 @@ cos:
 	b 3f
 2:
 	fdiv	d1, d1, d3  //divide to factorial
-	fadd	d4, d4, d1 
+	fadd	d2, d2, d1 
 	
 	fadd	d10, d10, d5
 	fmul	d3,	d3, d10
 	fadd	d10, d10, d5
 	fmul	d3, d3, d10
 3:
-	fsub	d8, d2, d4
+	fsub	d8, d4, d2
 	fcmp	d8, d7
 	bgt		0b
 	fmov	d0, d2
@@ -54,24 +58,43 @@ cos:
 	.equ	x, 16
 	.equ	p, 24
 	.equ	y, 32
-	.equ 	filename, 40
+	.equ 	progname, 40
+	.equ 	filename, 48
+	.equ 	filestruct, 56
 main:
-	ldr x0, [sp]
 	stp	x29, x30, [sp, #-32]!
 	mov	x29, sp
-	cmp x0, #2
-//	blt 2f //exit if no parameters were passed
+	cmp w0, #2
+	bne 2f //exit if no parameters were passed
 //	ldr x20, [sp, #16] //store name of the file
 //	str x20, [sp, filename]
 
-//test filename
+//	test filename
 //	adr x0, testmes
-// 	mov x1, x20
+// 	ldr x1, [x1, #8]
 //	bl printf
 
-	adr	x0, mes1
-	bl	printf
+	ldr x0, [x1]
+	str x0, [sp, progname]
+	ldr x0, [x1, #8]
+	str x0, [sp, filename]
+
+	ldr	x0, [sp, filename] 
+	adr x1, mode
+	bl fopen
+	cbnz x0, 1f 
+	ldr x0, [sp, #8]
+	bl perror
+	b 3f
 	
+1:
+	str x0, [sp, filestruct]
+	//test file writing
+	adr	x1, mes1
+	bl fprintf
+	
+	adr x0, mes1
+	bl	printf
 	adr	x0, mes2 //read x
 	add	x1, x29, x
 	bl	scanf
@@ -86,6 +109,9 @@ main:
 	ldr d0, [x29, x]
 	
 	bl	cos
+	ldr x0, [sp, filestruct]
+	bl fclose
+	
 	str	d0, [x29, p]
 	adr	x0, mes3
 	ldr	d0, [x29, x]
