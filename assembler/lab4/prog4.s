e1146f961ac0bf1d860b84890957f606f9ae8850
Dozen Sh <ohmytarik@mail.ru>
2022-09-11 20:02:46 +0300

    .

--- assembler/lab4/prog4.s
+++ assembler/lab4/prog4.s
@@ -21,21 +21,21 @@ usagemes:
 	.global	cos
 	.type	cos, %function
 cos:
-	//d7 - precision d0 - x
-	//ldr 	x11, [sp, filename]
-	ldr 	x0, [sp, filestruct]
-	mov 	x1, #0
+	//ldr 	x0, [sp, filestruct]
+	adr 	x1, filestr
+	mov		x23, #0
+	mov 	x2, #0
 	fmov	d1, d0
 	fmov 	d0, #1.0
 	fmov 	d5, d0
 	fadd	d10, d1, d0
 	fadd	d3, d1, d0
 0:
-	add		x1, x1, #1
+	add		x2, x2, #1
 	fmov	d4, d0 //previous sum
 	fmul	d1, d1, d1 //store multiplication
 	fmov	d6, d1 //save abs value
-1:
+	
 	fdiv	d1, d1, d3  //divide to factorial
 	fneg 	d1, d1
 	fadd	d0, d0, d1 
@@ -44,15 +44,29 @@ cos:
 	fmul	d3,	d3, d10
 	fadd	d10, d10, d5
 	fmul	d3, d3, d10
-	
+	b 2f
+
+1:
+	adr x0, mes12
+	bl printf
+	cmp 	x22, x23
+	bgt		0b
+	b 		5f
+
+
 2:
 	fsub	d8, d4, d0
+	//ldr		x0, [sp, filestruct]
 	fcmp	d8, d7
-	//fwrite here, debug calculations
 	//d0 - current sum
-	//adr x0, filestr
-	//bl fprintf
-	bgt		0b
+	bgt		3f
+3:
+	mov 	x22, #1
+	b 1b
+4:
+	mov		x22, #0
+	b 1b
+5:
 	fmov	d0, d4
 	ret
 	.size	cos, .-cos
@@ -61,16 +75,15 @@ cos:
 	.equ	x, 16
 	.equ	p, 24
 	.equ	y, 32
-	.equ 	progname, 40
-	.equ 	filename, 48
-	.equ 	filestruct, 56
+	.equ	tmp, 40
+	.equ 	progname, 48
+	.equ 	filename, 56
+	.equ 	filestruct, 64
 main:
 	stp	x29, x30, [sp, #-32]!
 	mov	x29, sp
 	cmp w0, #2
 	bne 2f //exit if no parameters were passed
-//	ldr x20, [sp, #16] //store name of the file
-//	str x20, [sp, filename]
 
 	ldr x0, [x1]
 	str x0, [sp, progname]
@@ -87,7 +100,7 @@ main:
 	
 1:
 	str x0, [sp, filestruct]
-	
+
 	adr x0, mes1
 	bl	printf
 	adr	x0, mes2 //read x
